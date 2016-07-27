1b4a5776bf12da4d8fabc1753460b11823301f16
mentaltoy <Nicola.Selleri@fao.org>
2016-07-27 17:29:01 +0200

    Style cleaning

--- config/config.js
+++ config/config.js
@@ -14,7 +14,7 @@ define(function () {
 
         //Top Menu configuration
         TOP_MENU_CONFIG: 'config/submodules/fx-menu/top_menu.json',
-        TOP_MENU_TEMPLATE: 'fx-menu/html/blank-fluid.html',
+        TOP_MENU_TEMPLATE: 'fx-menu/html/side.html',
 
         TOP_MENU_AUTH_MODE_HIDDEN_ITEMS: ['login'],
         TOP_MENU_PUBLIC_MODE_HIDDEN_ITEMS: ['logout', "download"],
